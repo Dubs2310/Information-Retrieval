c17de22ef936322426df9e24adc77ad364a18dc4
Dubs2310 <dubs2310@gmail.com>
2024-09-20 19:07:53 -0700

    Added comments and updated labels

--- assignment_1/indexing.py
+++ assignment_1/indexing.py
@@ -1,22 +1,23 @@
-#-------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------------------------
 # AUTHOR: Abdullah Irfan Siddiqui
 # FILENAME: indexing.py
 # SPECIFICATION: A program to index terms in a document and printing out the tf-idf weights of the terms relative to their documents
 # FOR: CS 5180- Assignment #1
 # TIME SPENT: 5 hours
-#-----------------------------------------------------------*/
+#---------------------------------------------------------------------------------------------------------------------------------------------
 
-# Importing some Python libraries
+# Importing some Python libraries ------------------------------------------------------------------------------------------------------------
 import math, csv
 documents = []
 
+# Function to print the documents (with labels) and their contents
 def print_docs(message, documents):
     print(message)
     for i in range(len(documents)):
         print(f'\td{i + 1}.', documents[i])
     print()
 
-# Reading the data in a csv file
+# Reading the data in a csv file -------------------------------------------------------------------------------------------------------------
 with open('collection.csv', 'r') as csvfile:
     reader = csv.reader(csvfile)    
     for i, row in enumerate(reader):
@@ -25,7 +26,7 @@ with open('collection.csv', 'r') as csvfile:
 
 print_docs("\nPrinting all documents (original):", documents)
 
-# Conducting stopword removal for pronouns/conjunctions. Hint: use a set to define your stopwords.
+# Conducting stopword removal for pronouns/conjunctions. Hint: use a set to define your stopwords. -------------------------------------------
 stop_words = {'i', 'and', 'she', 'her', 'they', 'their'}
 print("Stopwords:", stop_words)
 
@@ -39,9 +40,10 @@ for i in range(len(documents)):
 print_docs("Printing all documents (after stopward removal):", documents)
 
 
-# Conducting stemming. Hint: use a dictionary to map word variations to their stem.
+# Conducting stemming. Hint: use a dictionary to map word variations to their stem. ----------------------------------------------------------
 stemming = {}
 
+# Function to check if a stem exists for a word, and to return the stem if found or None, with a status message of success or failure
 def does_stem_exist_for_(word):
     stems = stemming.keys()
     if not stems:
@@ -72,6 +74,7 @@ for doc in documents:
 
 print("Stem variations map:", stemming)
 
+# Function to retrieve the stem of a word
 def get_stem_for_(word):
     for stem, variations in stemming.items():
         if word == stem or word in variations:
@@ -86,7 +89,7 @@ for i in range(len(documents)):
 
 print_docs("Printing all documents (after stemming):", documents)
 
-# Identifying the index terms.
+# Identifying the index terms. ----------------------------------------------------------------------------------------------------------------
 terms = []
 for doc in documents:
     words = doc.split()
@@ -96,49 +99,55 @@ for doc in documents:
 
 print("Printing all terms:", terms, '\n')
 
-# Building the document-term matrix by using the tf-idf weights.
-term_count_matrix = [
+# Building the document-term matrix by using the tf-idf weights. ------------------------------------------------------------------------------
+
+# Calculating the term counts per document
+term_counts = [
     [doc.split().count(term) for term in terms]
     for doc in documents
 ]
 
-term_frequency_matrix = [
+# Calculating the term frequencies per document, i.e. the normalized term counts per document
+term_frequencies = [
     [term_count / sum(term_count_row) for term_count in term_count_row]
-    for term_count_row in term_count_matrix
+    for term_count_row in term_counts
 ]
 
-document_frequency_matrix = [[]]
+# Calculating the document frequency per term, i.e. the number of documents that contain the term
+document_frequencies = [[]]
 for term in terms:
     document_frequency = 0
     for doc in documents:
         if term in doc:
             document_frequency += 1
-    document_frequency_matrix[0].append(document_frequency)
+    document_frequencies[0].append(document_frequency)
 
-inverse_document_frequency_matrix = [
-    [math.log10(len(documents) / document_frequency) for document_frequency in document_frequency_matrix[0]]
+# Calculating the inverse document frequencies, i.e. the inverse log ratio of the total number of documents to the document frequency per term
+inverse_document_frequencies = [
+    [math.log10(len(documents) / document_frequency) for document_frequency in document_frequencies[0]]
 ]
 
-term_frequency_inverse_document_frequency_matrix = [
-    [(term_frequency_row[i] * inverse_document_frequency_matrix[0][i]) for i in range(len(term_frequency_row))] 
-    for term_frequency_row in term_frequency_matrix
+# Calculating the term frequency - inverse document frequency weights per term
+term_frequency_inverse_document_frequencies = [
+    [(term_frequency_row[i] * inverse_document_frequencies[0][i]) for i in range(len(term_frequency_row))] 
+    for term_frequency_row in term_frequencies
 ]
 
-# Printing the document-term matrix.
+# Printing the document-term matrix. ----------------------------------------------------------------------------------------------------------
 def print_doc_term_matrix(metric, doc_term_matrix):
     print(
-        ("{:<30} " * (len(doc_term_matrix[0]) + 1)).format(
+        ("{:<35} " * (len(doc_term_matrix[0]) + 1)).format(
             f"\nDocument-Term Matrix ({metric})", 
             *(f'Term {i + 1} - "{terms[i]}"' for i in range(len(terms)))
         )
     )
     for i in range(len(doc_term_matrix)):
         row = doc_term_matrix[i]
-        print(("{:<30} " * (len(row) + 1)).format(f"d{i + 1}", *row))
+        print(("{:<35} " * (len(row) + 1)).format(f"d{i + 1}" if not metric in ["df", "idf"] else "D", *row))
     print()
 
-print_doc_term_matrix("count", term_count_matrix)
-print_doc_term_matrix("tf", term_frequency_matrix)
-print_doc_term_matrix("df", document_frequency_matrix)
-print_doc_term_matrix("idf", inverse_document_frequency_matrix)
-print_doc_term_matrix("tf-idf", term_frequency_inverse_document_frequency_matrix)
+print_doc_term_matrix("count", term_counts)
+print_doc_term_matrix("tf", term_frequencies)
+print_doc_term_matrix("df", document_frequencies)
+print_doc_term_matrix("idf", inverse_document_frequencies)
+print_doc_term_matrix("tf-idf", term_frequency_inverse_document_frequencies)
