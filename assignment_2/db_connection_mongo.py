fe3fa13f374cb57ac8fe0eab5719d449b3181841
Dubs2310 <dubs2310@gmail.com>
2024-10-08 15:53:54 -0700

    Finished Assignment 2

--- assignment_2/db_connection_mongo.py
+++ assignment_2/db_connection_mongo.py
@@ -2,96 +2,210 @@ from pymongo import MongoClient
 import datetime
 
 def connectDataBase():
-
-    # Creating a database connection object using pymongo
-
     DB_NAME = "CPP"
     DB_HOST = "localhost"
     DB_PORT = 27017
-
     try:
-
         client = MongoClient(host=DB_HOST, port=DB_PORT)
         db = client[DB_NAME]
-
         return db
-
     except:
         print("Database not connected successfully")
 
+
 def createUser(col, id, name, email):
-
-    # Value to be inserted
-    user = {"_id": id,
-            "name": name,
-            "email": email,
-            }
-
-    # Insert the document
+    user = {
+        "_id": id,
+        "name": name,
+        "email": email,
+    }
     col.insert_one(user)
 
+
 def updateUser(col, id, name, email):
-
-    # User fields to be updated
     user = {"$set": {"name": name, "email": email} }
-
-    # Updating the user
     col.update_one({"_id": id}, user)
 
-def deleteUser(col, id):
 
-    # Delete the document from the database
+def deleteUser(col, id):
     col.delete_one({"_id": id})
 
+
 def getUser(col, id):
-
     user = col.find_one({"_id":id})
-
     if user:
         return str(user['_id']) + " | " + user['name'] + " | " + user['email']
     else:
         return []
 
+
 def createComment(col, id_user, dateTime, comment):
-
-    # Comments to be included
-    comments = {"$push": {"comments": {
-                                       "datetime": datetime.datetime.strptime(dateTime, "%m/%d/%Y %H:%M:%S"),
-                                       "comment": comment
-                                       } }}
-
-    # Updating the user document
+    comments = {
+        "$push": {
+            "comments": {
+                "datetime": datetime.datetime.strptime(dateTime, "%m/%d/%Y %H:%M:%S"),
+                "comment": comment
+            }
+        }
+    }
     col.update_one({"_id": id_user}, comments)
+
 
 def updateComment(col, id_user, dateTime, new_comment):
-
-    # User fields to be updated
     comment = {"$set": {"comments.$.comment": new_comment} }
-
-    # Updating the user
     col.update_one({"_id": id_user, "comments.datetime": datetime.datetime.strptime(dateTime, "%m/%d/%Y %H:%M:%S")}, comment)
 
+
 def deleteComment(col, id_user, dateTime):
-
-    # Comments to be delete
     comments = {"$pull": {"comments": {"datetime": datetime.datetime.strptime(dateTime, "%m/%d/%Y %H:%M:%S")} }}
-
-    # Updating the user document
     col.update_one({"_id": id_user}, comments)
 
+
 def getChat(col):
-
-    # creating a document for each message
     pipeline = [
-                 {"$unwind": { "path": "$comments" }},
-                 {"$sort": {"comments.datetime": 1}}
-               ]
-
+        {"$unwind": { "path": "$comments" }},
+        {"$sort": {"comments.datetime": 1}}
+    ]
     comments = col.aggregate(pipeline)
-
     chat = ""
-
     for com in comments:
         chat += com['name'] + " | " + com['comments']['comment'] + " | " + str(com['comments']['datetime']) + "\n"
-
     return chat
+
+
+
+def createDocument(col, id, text, title, date, category):
+    doc = {
+        "_id": id,
+        "text": text,
+        "title": title,
+        "date": datetime.datetime.strptime(date, "%Y-%m-%d"),
+        "category": category
+    }
+    col.insert_one(doc)
+
+
+def updateDocument(col, id, text, title, date, category):
+    where = { "_id": id }
+    set = {
+        "$set": {
+            "text": text,
+            "title": title,
+            "date": datetime.datetime.strptime(date, "%Y-%m-%d"),
+            "category": category
+        }
+    }
+    col.update_one(where, set)
+
+
+def deleteDocument(col, id):
+    where = { "_id": id }
+    col.delete_one(where)
+
+def getIndex(col):
+    pipeline = [
+        { 
+            "$project": { 
+                "terms": {
+                    "$split": [
+                        {
+                            "$toLower": { 
+                                "$replaceAll": { 
+                                    "input": {
+                                        "$replaceAll": { 
+                                            "input": {
+                                                "$replaceAll": { 
+                                                    "input": {
+                                                        "$replaceAll": { 
+                                                            "input": "$text", 
+                                                            "find": "?", 
+                                                            "replacement": "" 
+                                                        }
+                                                    }, 
+                                                    "find": "!", 
+                                                    "replacement": "" 
+                                                }
+                                            }, 
+                                            "find": ",", 
+                                            "replacement": "" 
+                                        }
+                                    }, 
+                                    "find": ".", 
+                                    "replacement": "" 
+                                }
+                            }
+                        },
+                        " "
+                    ] 
+                }, 
+                "title": 1,
+                "_id": 0
+            } 
+        },
+        { "$unwind": "$terms" },
+        {
+            "$group": {
+                "_id": {
+                    "title": "$title",
+                    "term": "$terms"
+                },
+                "term_count": { "$count": {} },
+            }
+        },
+        {
+            "$project": {
+                "term": "$_id.term",
+                "title_term_count_map": { 
+                    "$concat": ["$_id.title", ":", {"$toString" : "$term_count"}] 
+                },
+                "_id": 0,
+            }
+        },
+        { 
+            "$group": { 
+                "_id": "$term",
+                "term_count_per_title_array": { "$push": "$title_term_count_map" }
+            }
+        },
+        {
+            "$addFields": {
+                "term_counts_per_title": {
+                    "$reduce": {
+                        "input": "$term_count_per_title_array",
+                        "initialValue": "",
+                        "in": {
+                            "$cond": {
+                                "if": { "$eq": [ { "$indexOfArray": [ "$term_count_per_title_array", "$$this" ] }, 0 ] },
+                                "then": { "$concat": [ "$$value", "$$this" ] },
+                                "else": { "$concat": [ "$$value", ", ", "$$this" ] }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        {
+            "$project": {
+                "kvObject": { 
+                    "k": "$_id", 
+                    "v": "$term_counts_per_title" 
+                },
+                "_id": 0
+            }
+        },
+        {
+            "$group": {
+                "_id": "null",
+                "kvObjects": { "$push": "$kvObject" }
+            }
+        },
+        {
+            "$project": {
+                "results": {
+                    "$arrayToObject": "$kvObjects"
+                }
+            }
+        }
+    ]
+    for result in col.aggregate(pipeline):
+        return result["results"]
